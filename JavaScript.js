6210931f4c5c9368f63d726d2997fdb4da9fe978
Luis Barreiro <2161323@my.ipleiria.pt>
2017-12-14 22:13:11 +0000

    first commit

---
+++ JavaScript.js
@@ -0,0 +1,76 @@
+﻿$(document).ready(function () {
+    $("#getAllEntries").click(() => {
+        getAirEntries("", "/api/All");
+    });
+    $("#airEntryLeiria").click(() => {
+        getAirEntries("Leiria", "/api/city/");
+    });
+    $("#airEntryPorto").click(() => {
+        getAirEntries("Porto", "/api/city/");
+    });
+    $("#airEntryCoimbra").click(() => {
+        getAirEntries("Coimbra", "/api/city/");
+    });
+    $("#airEntryLisboa").click(() => {
+        getAirEntries("Lisboa", "/api/city/");
+    });
+    $("#bSubmit").click(() => {
+        if ($("#bstartDate").val() > $("#bendDate").val()) {
+            alert("Data inicial deve ser menor que a data final ");
+        } else {
+            var jsonForm = { form: $("#dailyForm").serialize() };
+            console.log("/api/dailyResume/" + $("#bElem").val() + "/" + $("#bCity").val() + "?dateIn=" + $("#bstartDate").val() + "&dateOut=" + $("#bendDate").val());
+            $.get("/api/dailyResume/" + $("#bElem").val() + "/" + $("#bCity").val() + "?dateIn=" + $("#bstartDate").val() + "&dateOut=" + $("#bendDate").val(), (data, status) => {
+                console.log(JSON.stringify(data));
+                $("#DivBody").html("");
+                var html = '<table class="table"><thead>';
+                html += '<th>Maximum Value</th>';
+                html += '<th>Minimum Value</th>';
+                html += '<th>Average Value</th>';
+                html += '<th>Location</th>';
+                html += '<th>Resume Date</th>';
+                html += "</thead>";
+                html += "<tbody>";
+                for (i = 0; i < data.length; i++) {
+                    html += "<tr>";
+                    html += '<td>' + data[i]["Max"] + '</td>';
+                    html += '<td >' + data[i]["Min"] + '</td>';
+                    html += '<td>' + data[i]["Avg"] + '</td>';
+                    html += '<td>' + data[i]["City"] + '</td>';
+                    html += '<td>' + data[i]["Date"] + '</td>';
+                    html += "</tr>";
+                }
+                html += "</tbody>";
+                html += "</table>";
+                $("#DivBody").append(html);
+            });
+        }
+    });
+    function getAirEntries(city, url) {
+        $("#DivBody").html("");
+        console.log(city);
+        var baseUrl = document.location.origin;
+        $.get(baseUrl + url + city, function (data, status) {
+            var html = '<table class="table"><thead>';
+            html += '<th>CityElementID</th>';
+            html += '<th>Element</th>';
+            html += '<th>ElementValue</th>';
+            html += '<th>City</th>';
+            html += '<th>Date</th>';
+            html += "</thead>";
+            html += "<tbody>";
+            for (i = 0; i < data.length; i++) {
+                html += "<tr>";
+                html += '<td>' + data[i]["CityElementID"] + '</td>';
+                html += '<td >' + data[i]["Element"] + '</td>';
+                html += '<td>' + data[i]["ElementValue"] + '</td>';
+                html += '<td>' + data[i]["Location"] + '</td>';
+                html += '<td>' + data[i]["Date"] + '</td>';
+                html += "</tr>";
+            }
+            html += "</tbody>";
+            html += "</table>";
+            $("#DivBody").append(html);
+        });
+    }
+});
